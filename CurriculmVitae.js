e93f2fa9447cdd7c68640a83812ca68044f5eb81
Norberto J. Collazo <norberto.collazo@upr.edu>
2022-04-01 14:37:12 -0400

    implemente la libreria de papaparse para leer documentos de csv en javascript

---
+++ CurriculmVitae.js
@@ -0,0 +1,53 @@
+class ContactInfo {
+	constructor(Fullname, Address, Phone, Email){
+		this.Fullname = Fullname;
+		this.Address = Address;
+		this.Phone = Phone;
+		this.Email = Email;
+	}
+	present(){
+		return ' '
+	}
+}
+class Education {
+	constructor(time, school, degree){
+		this.time = time;
+		this.degree = degree;
+		this.school = school;
+	}
+}
+class Experience extends Education {
+	constructor(time, title, achieve, agency){
+		super(time);
+		this.title= title;
+		this.achieve = achieve;
+		this.agency=agency;
+	}
+}
+class Extra extends Experience{
+	constructor(title,Res_int,Cert_prog, Cert, Skill,Lang){
+		super(title);
+		this.RIntrest=Res_int;
+		this.Certificate=Cert;
+		this.Certificate_Prog=Cert_prog;
+		this.Skill=Skill;
+		this.language=Lang;
+
+	}
+	
+}
+
+
+const uploadconfirm= document.getElementById('uploadconfirm').
+			addEventListener('click', ()=> {
+				Papa.parse(document.getElementById('uploadfile').files[0],
+				{
+					download: true,
+					header: true,
+					skipEmptyLines: true,
+					complete: function(results){
+						console.log(results);
+						
+					}
+				});
+			});
