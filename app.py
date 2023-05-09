5c942c5bbee69b924777211c2fde7d1ced920dfc
Zuleinis Ramos <zuleinis.ramos@upr.edu>
2023-05-09 18:19:28 -0400

    add simple form code

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('form.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
